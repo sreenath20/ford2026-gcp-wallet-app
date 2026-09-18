9d9bae82f9c7bedf0e1f2fc0450f2ba796a63e40
sreenath <rdgowda79@gmail.com>
2026-09-18 13:11:21 +0530

    allow public crossorigin @ wallet controller

--- src/main/java/com/datajpa/demo/wallet/WalletController.java
+++ src/main/java/com/datajpa/demo/wallet/WalletController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("v1/wallets")
-@CrossOrigin(origins = "http://localhost:4200/")
+@CrossOrigin// allows public origins
 public class WalletController {
 
     @GetMapping
